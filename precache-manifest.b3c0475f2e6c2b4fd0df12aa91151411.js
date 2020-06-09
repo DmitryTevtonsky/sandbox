72855e71d82ec32e83775e06dd4d9bc9e6202559
tevtonsky1054m1@gmail.com <tevtonsky1054m1@gmail.com>
2020-06-09 21:42:58 +0300

    Updates

---
+++ precache-manifest.b3c0475f2e6c2b4fd0df12aa91151411.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1a7697b35ffd3f356e32bb9825597e",
+    "url": "/sandbox/index.html"
+  },
+  {
+    "revision": "23ef76925e0111fa3db3",
+    "url": "/sandbox/static/css/2.d4ffb283.chunk.css"
+  },
+  {
+    "revision": "2966ece79aa6a984c8f8",
+    "url": "/sandbox/static/css/main.ed2cff6e.chunk.css"
+  },
+  {
+    "revision": "23ef76925e0111fa3db3",
+    "url": "/sandbox/static/js/2.41a30e98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sandbox/static/js/2.41a30e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2966ece79aa6a984c8f8",
+    "url": "/sandbox/static/js/main.c9dbb4fa.chunk.js"
+  },
+  {
+    "revision": "67608fc84a22135aab94",
+    "url": "/sandbox/static/js/runtime-main.edc2f877.js"
+  }
+]);
